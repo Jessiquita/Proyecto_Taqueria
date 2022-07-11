f2a5f62daa15d3938a4ca48935030d85d41c6fea
Upqroo <Upqroo@DESKTOP-4GNQQK1>
2022-07-11 18:29:20 -0500

    Agregar archivos de proyecto.

---
+++ Context/AplicationdbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Taqueria.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Taqueria.Context
+{
+    public class AplicationdbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server = localhost; database = proyecto_Taqueria; user= root; pasword= ");
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
